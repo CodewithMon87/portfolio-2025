5e4037c2c88889d6aefe6be62ae0b81da7e339ac
Raymond Sabado <raymond.sabado87@gmail.com>
2025-11-05 01:28:13 +0800

    add typing effect in hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,67 +1,63 @@
-"use client";
+'use client'
 
-import React from "react";
-import Image from "next/image";
-import { Link } from "react-scroll/modules";
-import { HiArrowDown } from "react-icons/hi";
-import { TypeAnimation } from "react-type-animation";
+import React from 'react'
+import Image from 'next/image'
+import { Link } from 'react-scroll/modules'
+import { HiArrowDown } from 'react-icons/hi'
+import { TypeAnimation } from 'react-type-animation'
 
 const HeroSection = () => {
   return (
-    <section id="home">
-      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+    <section id='home'>
+      <div className='flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left'>
         {/* Left: Image */}
-        <div className="md:mt-2 md:w-1/2">
+        <div className='md:mt-2 md:w-1/2'>
           <Image
-            src="/headshot.png"
-            alt="Raymond Headshot"
+            src='/headshot.png'
+            alt='Raymond Headshot'
             width={325}
             height={325}
-            className="rounded-full shadow-2xl"
+            className='rounded-full shadow-2xl'
           />
         </div>
 
         {/* Right: Typing Effect Text */}
-        <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
+        <div className='md:mt-2 md:w-3/5'>
+          <h1 className='text-4xl font-bold mt-6 md:mt-0 md:text-7xl'>
             <TypeAnimation
               sequence={[
                 "Hi, I'm Raymond!", // type this
                 2000, // wait 2 seconds
-                "", // clear text
+                '', // clear text
                 500,
-                "Hi, I'm Raymond!", // type again
+                "Hi, I'm Raymond!" // type again
               ]}
               speed={50}
-              wrapper="span"
+              wrapper='span'
               repeat={Infinity}
             />
           </h1>
 
-          <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&apos;m a{" "}
+          <p className='text-lg mt-4 mb-6 md:text-2xl'>
+            I&apos;m a{' '}
             <TypeAnimation
               sequence={[
-                "Software Engineer",
-                2000,
-                "Frontend Developer",
-                2000,
-                "Full Stack Engineer",
-                2000,
+                "I'm a Frontend Developer based in Los Angeles, CA. Working towards creating software that makes life easier and more meaningful.",
+                2000, // wait before restarting
+                '',
+                500 // small pause before typing again
               ]}
-              wrapper="span"
+              wrapper='p'
               speed={50}
               repeat={Infinity}
-              className="font-semibold text-teal-600"
-            />{" "}
-            based in Los Angeles, CA. Working towards creating software that
-            makes life easier and more meaningful.
+              className='text-lg mt-4 mb-6 md:text-2xl font-normal'
+            />
           </p>
 
           <Link
-            to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-            activeClass="active"
+            to='projects'
+            className='text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700'
+            activeClass='active'
             spy={true}
             smooth={true}
             offset={-100}
@@ -73,20 +69,20 @@ const HeroSection = () => {
       </div>
 
       {/* Scroll Down Arrow */}
-      <div className="flex flex-row items-center text-center justify-center">
+      <div className='flex flex-row items-center text-center justify-center'>
         <Link
-          to="about"
-          activeClass="active"
+          to='about'
+          activeClass='active'
           spy={true}
           smooth={true}
           offset={-100}
           duration={500}
         >
-          <HiArrowDown size={35} className="animate-bounce" />
+          <HiArrowDown size={35} className='animate-bounce' />
         </Link>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default HeroSection;
+export default HeroSection
