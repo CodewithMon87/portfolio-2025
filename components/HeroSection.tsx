9d8a61dea000aa32cfefc56901c86bc3212fb352
Raymond Sabado <raymond.sabado87@gmail.com>
2025-11-05 01:21:41 +0800

    add Animation

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,32 +1,63 @@
-"use client" // this is a client component
-import React from "react"
-import Image from "next/image"
-import { Link } from "react-scroll/modules"
-import { HiArrowDown } from "react-icons/hi"
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { Link } from "react-scroll/modules";
+import { HiArrowDown } from "react-icons/hi";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+        {/* Left: Image */}
         <div className="md:mt-2 md:w-1/2">
           <Image
             src="/headshot.png"
-            alt=""
+            alt="Raymond Headshot"
             width={325}
             height={325}
             className="rounded-full shadow-2xl"
           />
         </div>
+
+        {/* Right: Typing Effect Text */}
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Raymond!</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
+            <TypeAnimation
+              sequence={[
+                "Hi, I'm Raymond!", // type this
+                2000, // wait 2 seconds
+                "", // clear text
+                500,
+                "Hi, I'm Raymond!", // type again
+              ]}
+              speed={50}
+              wrapper="span"
+              repeat={Infinity}
+            />
+          </h1>
+
           <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&#39;m a{" "}
-            <span className="font-semibold text-teal-600">
-              Software Engineer{" "}
-            </span>
+            I&apos;m a{" "}
+            <TypeAnimation
+              sequence={[
+                "Software Engineer",
+                2000,
+                "Frontend Developer",
+                2000,
+                "Full Stack Engineer",
+                2000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+              className="font-semibold text-teal-600"
+            />{" "}
             based in Los Angeles, CA. Working towards creating software that
             makes life easier and more meaningful.
           </p>
+
           <Link
             to="projects"
             className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
@@ -40,7 +71,9 @@ const HeroSection = () => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-row items-center text-center justify-center ">
+
+      {/* Scroll Down Arrow */}
+      <div className="flex flex-row items-center text-center justify-center">
         <Link
           to="about"
           activeClass="active"
@@ -53,7 +86,7 @@ const HeroSection = () => {
         </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
