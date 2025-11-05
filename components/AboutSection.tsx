a7a2a17d64f7fe111ae2df6eaf6da44caf32e918
Raymond Sabado <raymond.sabado87@gmail.com>
2025-11-05 22:47:23 +0800

    add projects

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,74 +1,77 @@
-import React from "react"
-import Image from "next/image"
+import React from 'react'
+import Image from 'next/image'
 
 const skills = [
-  { skill: "HTML" },
-  { skill: "CSS" },
-  { skill: "JavaScript" },
-  { skill: "TypeScript" },
-  { skill: "Python" },
-  { skill: "React" },
-  { skill: "Next.js" },
-  { skill: "Tailwind CSS" },
-  { skill: "Git" },
-  { skill: "GitHub" },
-  { skill: "Jupyter Notebooks" },
+  { skill: 'HTML' },
+  { skill: 'CSS' },
+  { skill: 'SCSS' },
+  { skill: 'SaSS' },
+  { skill: 'Tailwind CSS' },
+  { skill: 'Bootstrap' },
+  { skill: 'Foundation' },
+  { skill: 'JavaScript' },
+  { skill: 'TypeScript' },
+  { skill: 'ReactJs' },
+  { skill: 'Next.js' },
+  { skill: 'Laravel' },
+  { skill: 'Git' },
+  { skill: 'GitHub' },
+  { skill: 'Wordpress' },
+  { skill: 'Shopify' }
 ]
 
 const AboutSection = () => {
   return (
-    <section id="about">
-      <div className="my-12 pb-12 md:pt-16 md:pb-48">
-        <h1 className="text-center font-bold text-4xl">
+    <section id='about'>
+      <div className='my-12 pb-12 md:pt-16 md:pb-48'>
+        <h1 className='text-center font-bold text-4xl'>
           About Me
-          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+          <hr className='w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded'></hr>
         </h1>
 
-        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
-          <div className="md:w-1/2 ">
-            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+        <div className='flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left'>
+          <div className='md:w-1/2 '>
+            <h1 className='text-center text-2xl font-bold mb-6 md:text-left'>
               Get to know me!
             </h1>
             <p>
-              Hi, my name is Hosna and I am a{" "}
-              <span className="font-bold">{"highly ambitious"}</span>,
-              <span className="font-bold">{" self-motivated"}</span>, and
-              <span className="font-bold">{" driven"}</span> software engineer
-              based in Los Angeles, CA.
+              Hi, I’m Raymond, an experienced Frontend Developer passionate
+              about crafting responsive and user-friendly web experiences. I
+              specialize in HTML, CSS, and JavaScript, and I’m proficient in
+              modern frameworks like ReactJS, enabling me to build dynamic,
+              scalable, and high-performance interfaces that elevate user
+              experience.
             </p>
             <br />
             <p>
-              I graduated from California State University, Northridge in 2019
-              with a BS in Computer Engineering and have been working in the
-              field ever since.
+              I earned my Bachelor’s degree in Information Technology from the
+              University of the East in 2010 and have been continuously growing
+              in the tech industry ever since.
             </p>
             <br />
             <p>
-              I have a wide range of hobbies and passions that keep me busy.
-              From reading, playing sports, traveling, to making YouTube videos,
-              I am always seeking new experiences and love to keep myself
-              engaged and learning new things.
+              Beyond coding, I enjoy exploring my hobbies—reading, playing
+              sports, and video gaming. I’m always eager to learn new things and
+              take on challenges that inspire creativity and growth.
             </p>
             <br />
             <p>
-              I believe that you should{" "}
-              <span className="font-bold text-teal-500">
-                never stop growing
-              </span>{" "}
-              and that&#39;s what I strive to do, I have a passion for
-              technology and a desire to always push the limits of what is
-              possible. I am excited to see where my career takes me and am
-              always open to new opportunities. 🙂
+              I believe growth{' '}
+              <span className='font-bold text-teal-500'>never stops</span> and
+              that&#39;s what I strive to do, I have a passion for technology
+              and a desire to always push the limits of what is possible. I am
+              excited to see where my career takes me and am always open to new
+              opportunities. 🙂
             </p>
           </div>
-          <div className="text-center md:w-1/2 md:text-left">
-            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
-            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+          <div className='text-center md:w-1/2 md:text-left'>
+            <h1 className='text-2xl font-bold mb-6'>My Skills</h1>
+            <div className='flex flex-wrap flex-row justify-center z-10 md:justify-start'>
               {skills.map((item, idx) => {
                 return (
                   <p
                     key={idx}
-                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                    className='bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold'
                   >
                     {item.skill}
                   </p>
@@ -76,11 +79,11 @@ const AboutSection = () => {
               })}
             </div>
             <Image
-              src="/hero-image.png"
-              alt=""
+              src='/hero-image.png'
+              alt=''
               width={325}
               height={325}
-              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+              className='hidden md:block md:relative md:bottom-4 md:left-32 md:z-0'
             />
           </div>
         </div>
