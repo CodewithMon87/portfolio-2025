b5fba31405bd806b0acdd82399a6540a9e4e3caf
Raymond Sabado <raymond.sabado87@gmail.com>
2025-11-05 15:29:58 +0800

    add Logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
-"use client" // this is a client component
-import React from "react"
-import { useState } from "react"
-import { Link } from "react-scroll/modules"
-import { usePathname } from "next/navigation"
-import { useTheme } from "next-themes"
-import { RiMoonFill, RiSunLine } from "react-icons/ri"
-import { IoMdMenu, IoMdClose } from "react-icons/io"
+'use client' // this is a client component
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-scroll/modules'
+import { usePathname } from 'next/navigation'
+import { useTheme } from 'next-themes'
+import { RiMoonFill, RiSunLine } from 'react-icons/ri'
+
+import { IoMdMenu, IoMdClose } from 'react-icons/io'
 
 interface NavItem {
   label: string
@@ -14,37 +15,41 @@ interface NavItem {
 
 const NAV_ITEMS: Array<NavItem> = [
   {
-    label: "Home",
-    page: "home",
+    label: 'Home',
+    page: 'home'
   },
   {
-    label: "About",
-    page: "about",
+    label: 'About',
+    page: 'about'
   },
   {
-    label: "Projects",
-    page: "projects",
-  },
+    label: 'Projects',
+    page: 'projects'
+  }
 ]
 
-export default function Navbar() {
+export default function Navbar () {
   const { systemTheme, theme, setTheme } = useTheme()
-  const currentTheme = theme === "system" ? systemTheme : theme
+  const currentTheme = theme === 'system' ? systemTheme : theme
   const pathname = usePathname()
   const [navbar, setNavbar] = useState(false)
   return (
-    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
-      <div className="justify-between md:items-center md:flex">
+    <header className='w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600'>
+      <div className='justify-between md:items-center md:flex'>
         <div>
-          <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link to="home">
-              <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold">Raymond Sabado</h2>
+          <div className='flex items-center justify-between py-3 md:py-5 md:block'>
+            <Link to='home'>
+              <div className='container flex items-center space-x-2'>
+                <img
+                  src='/raylogo.png' // ✅ path from the public folder
+                  alt='CodewithRay Logo'
+                  className='h-10 w-auto' // adjust the height if needed
+                />
               </div>
             </Link>
-            <div className="md:hidden">
+            <div className='md:hidden'>
               <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
                 onClick={() => setNavbar(!navbar)}
               >
                 {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
@@ -56,19 +61,19 @@ export default function Navbar() {
         <div>
           <div
             className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? "block" : "hidden"
+              navbar ? 'block' : 'hidden'
             }`}
           >
-            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            <div className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
               {NAV_ITEMS.map((item, idx) => {
                 return (
                   <Link
                     key={idx}
                     to={item.page}
                     className={
-                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100 cursor-pointer"
+                      'block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100 cursor-pointer'
                     }
-                    activeClass="active"
+                    activeClass='active'
                     spy={true}
                     smooth={true}
                     offset={-100}
@@ -79,17 +84,17 @@ export default function Navbar() {
                   </Link>
                 )
               })}
-              {currentTheme === "dark" ? (
+              {currentTheme === 'dark' ? (
                 <button
-                  onClick={() => setTheme("light")}
-                  className="bg-slate-100 p-2 rounded-xl"
+                  onClick={() => setTheme('light')}
+                  className='bg-slate-100 p-2 rounded-xl'
                 >
-                  <RiSunLine size={25} color="black" />
+                  <RiSunLine size={25} color='black' />
                 </button>
               ) : (
                 <button
-                  onClick={() => setTheme("dark")}
-                  className="bg-slate-100 p-2 rounded-xl"
+                  onClick={() => setTheme('dark')}
+                  className='bg-slate-100 p-2 rounded-xl'
                 >
                   <RiMoonFill size={25} />
                 </button>
