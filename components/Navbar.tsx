c8d34313a4dbece113e6597e0120167061395b5e
Raymond Sabado <raymond.sabado87@gmail.com>
2025-11-06 15:22:17 +0800

    update portfolio logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,9 +39,14 @@ export default function Navbar () {
           <div className='flex items-center justify-between py-3 md:py-5 md:block'>
             <Link to='home'>
               <div className='container flex items-center space-x-2'>
-                <h2 className='text-2xl font-bold'>🚀CodewithRay</h2>
+                <img
+                  src='/raylogo-updated.png'
+                  alt='CodeWithRay Logo'
+                  className='h-14 w-auto'
+                />
               </div>
             </Link>
+
             <div className='md:hidden'>
               <button
                 className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
